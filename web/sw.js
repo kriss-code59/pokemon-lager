6be168bc24033dc811b816f09769b0ba6a4fb2b3
kriss-code59 <norgekriss@gmail.com>
2026-08-06 23:10:08 +0200

    Bump til v=7 sa ingen sitter fast paa gammel app.js

--- web/sw.js
+++ web/sw.js
@@ -9,8 +9,8 @@
  *
  * Regelen for cache er enkel: skallet fra cache, /api/ ALDRI fra cache.
  */
-const CACHE = "pokepuls-skall-v6";
-const SKALL = ["/", "/style.css?v=6", "/app.js?v=6", "/ikon.svg", "/manifest.webmanifest"];
+const CACHE = "pokepuls-skall-v7";
+const SKALL = ["/", "/style.css?v=7", "/app.js?v=7", "/ikon.svg", "/manifest.webmanifest"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SKALL)).then(() => self.skipWaiting()));
