04d8a7c3205ed6d5ff89a8bc0a184303a8b92d25
kriss-code59 <norgekriss@gmail.com>
2026-08-07 22:17:21 +0200

    Produktsidene peker paa style.css v=8

--- api/sider.py
+++ api/sider.py
@@ -97,7 +97,7 @@ def _sidehode(tittel: str, beskrivelse: str, kanonisk: str, jsonld: dict | None,
 <meta name="twitter:card" content="summary_large_image">
 <link rel="manifest" href="/manifest.webmanifest">
 <link rel="icon" href="/ikon.svg" type="image/svg+xml">
-<link rel="stylesheet" href="/style.css?v=7">
+<link rel="stylesheet" href="/style.css?v=8">
 {ld}
 </head><body class="side">"""
 
